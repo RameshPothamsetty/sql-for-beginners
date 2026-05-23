1941484df65283443622fd98b2caecd0c0d3c8d0
POTHAMSETTY RAMESH <rap53748@gmail.com>
2026-05-23 13:08:29 +0530

    ✅ Add solution for: Categorizing Product Pricing Tiers - SQL

---
+++ exercises/CategorizingProductPricingTiersSQL.sql
@@ -0,0 +1,10 @@
+-- Write a SQL query below --
+SELECT name,
+ROUND(price , 2) AS price,
+    CASE
+        WHEN price > 200 THEN 'Premium'
+        WHEN price >= 100 AND price <= 200 THEN 'Mid-Range'
+        WHEN price < 100 THEN 'Budget'
+    END AS price_tier
+FROM products
+ORDER BY price DESC;
